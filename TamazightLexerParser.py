596107fca921c49f88c8faeb91618b3824a999a2
Jemmal soufiane <jemmalsoufiane@gmail.com>
2022-01-04 19:51:00 +0100

    Update TamazightLexerParser.py

--- TamazightLexerParser.py
+++ TamazightLexerParser.py
@@ -20,7 +20,10 @@ reserved = {
     'ilem': 'ilem',
     'agejdan': 'agejdan',
     'aru': 'aru',
-    'efk': 'efk'
+    'efk': 'efk',
+    'sehviver': 'sehviver',
+    'uslig': 'uslig',
+    'ma': 'ma'
 }
 
 tokens = [
@@ -32,7 +35,13 @@ tokens = [
     'DIVIDE',
     'MULTIPLY',
     'EQUALS',
-    'STRING'
+    'STRING',
+    'GT',
+    'LT',
+    'GE',
+    'LE',
+    'EE',
+    'NE'
 ] + list(reserved.values())
 
 literals = [',', ';', '(', ')', '{', '}']
@@ -47,14 +56,23 @@ t_taggayt = r'taggayt'
 t_urkid = r'urkid'
 t_ilem = r'ilem'
 t_agejdan = r'agejdan'
+t_uslig = r'uslig'
+t_sehviver = r'sehviver'
 t_aru = r'aru'
 t_efk = r'efk'
+t_ma = r'ma'
+t_GT = r'\>'
+t_LT = r'\<'
+t_GE = r'\>\='
+t_LE = r'\<\='
+t_EE = r'\=\='
+t_NE = r'\!\='
 
 t_ignore = r' '
 
 
 def t_STRING(t):
-    r'("[^"]*")|(\'[^\']*\')'
+    r'("[^"]")|(\'[^\']\')'
     t.value = str(t.value)
     return t
 
@@ -105,15 +123,54 @@ precedence = (
 )
 
 
+
 def p_code(p):
     '''
-    code : azayez taggayt NAME '{' main '}'
+    code : encaps taggayt NAME '{' inner_code '}'
+    '''
+
+
+def p_Encaps(p):
+    '''
+    encaps : azayez
+            | uslig
+            | sehviver
+            | empty
+    '''
+
+
+def p_inCode(p):
+    '''
+    inner_code :  attribut_statement functions_statement
+             | attribut_statement
+             | functions_statement
+    '''
+
+
+def p_attr_statement(p):
+    '''
+    attribut_statement : encaps NAME
+    '''
+
+
+
+def p_function(p):
+    '''
+    functions_statement : main functions_statement
+                        | method
+                        | empty
+    '''
+
+
+def p_method(p):
+    '''
+        method : encaps NAME '(' ')' '{' statements '}'
     '''
 
 
 def p_main(p):
     '''
-    main : azayez urkid ilem agejdan '(' ')' '{' statements '}'
+    main :   encaps urkid ilem agejdan '(' ')' '{' statements '}'
     '''
 
 
@@ -129,6 +186,7 @@ def p_statement(p):
     statement   : var_assign
                 | print_statement
                 | scanf_statement
+                | if_statement
                 | empty
     '''
     print(run(p[1]))
@@ -155,6 +213,51 @@ def p_scanfStat(p):
     p[0] = ('efk_statement', p[3])
 
 
+
+def p_ifStatement(p):
+    '''
+    if_statement : ma '(' condition ')' '{' con_statements '}'
+    '''
+    p[0] = ('if_statement', p[3], p[6])
+
+    # (0,1(0,1,2),2)
+
+
+def p_conStatements(p):
+    '''
+    con_statements : con_statement con_statements
+                    | empty
+    '''
+    p[0] = p[1]
+
+def p_conStatement(p):
+    '''
+        con_statement : var_assign
+                        | print_statement
+                        | scanf_statement
+                        | if_statement
+                        | empty
+    '''
+    p[0] = p[1]
+def p_condition(p):
+    '''
+        condition : expression comparaison expression
+    '''
+    p[0] = (p[2], p[1], p[3])
+
+
+
+def p_comparaison(p):
+    '''
+        comparaison : GT
+                    | LT
+                    | GE
+                    | LE
+                    | EE
+                    | NE
+    '''
+    p[0] = p[1]
+
 def p_expression(p):
     '''
     expression :  expression MULTIPLY expression
@@ -167,7 +270,7 @@ def p_expression(p):
 
 def p_expression_int_float(p):
     '''
-    expression : INT 
+    expression : INT
                 | FLOAT
     '''
     p[0] = p[1]
@@ -221,37 +324,62 @@ def run(p):
             if(p[1] not in env):
                 return 'Undeclared variable found!'
             else:
+                print("contenu")
                 return env[p[1]]
-        if(p[0] == 'aru_statement'):
+        elif p[0] == 'aru_statement':
             if(p[1] in env):
                 print(env[p[1]])
             else:
                 return run(p[1])
-        elif(p[0] == 'efk_statement'):
+        elif p[0] == 'efk_statement':
             s = input()
-            if(s.isnumeric()):
+            if s.isnumeric():
                 print("it s int")
                 env[p[1]] = int(s)
-            elif(s.isalnum()):
+            elif s.isalnum():
                 env[p[1]] = s
                 print("its string")
-            elif(isfloat(s)):
+            elif isfloat(s):
                 env[p[1]] = float(s)
                 print("it s float")
+        elif p[0] == 'if_statement':
+            if p[1][0] == "==":
+                if isinstance(p[1][2], int) and p[1][1][0] == 'var':
+                   if compare(env[p[1][1][1]], p[1][2]):
+                       print("dkhelt")
+                       run(p[2])
+
+                elif isinstance(p[1][2], float) and p[1][1][0] == 'var':
+                    print(env[p[1][1][1]])
+                    print(p[1][2])
+                elif p[1][1][0] == 'var' and p[1][2][0] == 'var':
+                    print(env[p[1][1][1]])
+                    print(env[p[1][2][1]])
+                else:
+                    print("condition error")
+            else:
+                print("error")
     else:
         return p
+
+
+def compare(a, b):
+    if a == b:
+        return True
+    return False
 
 
 parser = yacc.yacc()
 
 data = '''
-    azayez taggayt $className {
-        azayez urkid ilem agejdan(){
-            efk($a);
-            efk($b);
-            $c = $a+$b;
-            aru($c);
+     azayez taggayt $className {
+        uslig urkid ilem agejdan(){
+            $a=8;
+            ma( $a == 8 ){
+                 $a =10;
+            }  
         }
+        
     }
 '''
 
@@ -274,3 +402,41 @@ parser.parse(data)
 #     parser.parse(s)
 
 ### < / P A R S E R > ###
+
+
+# elif p[0] == 'conditionSOS':
+# if p[0][1] == "==":
+#     if run(p[1][1]) == run(p[1][2]):
+#         run(p[2])
+# if p[0][1] == ">=":
+#     if run(p[1][1]) >= run(p[1][2]):
+#         run(p[2])
+# if p[0][1] == "<=":
+#     if run(p[1][1]) <= run(p[1][2]):
+#         run(p[2])
+# if p[0][1] == ">":
+#     if run(p[1][1]) > run(p[1][2]):
+#         run(p[2])
+# if p[0][1] == "<":
+#     if run(p[1][1]) < run(p[1][2]):
+#         run(p[2])
+# if p[0][1] == "!=":
+#     if run(p[1][1]) != run(p[1][2]):
+#         run(p[2])
+
+
+# elif p[1][0] == ">=":
+# if run(p[1][1]) >= run(p[1][2]):
+#     run(p[2])
+# elif p[1][0] == "<=":
+#     if run(p[1][1]) <= run(p[1][2]):
+#         run(p[2])
+#     elif p[1][0] == "<":
+#     if run(p[1][1]) < run(p[1][2]):
+#         run(p[2])
+#     elif p[1][0] == ">":
+#     if run(p[1][1]) > run(p[1][2]):
+#         run(p[2])
+#     elif p[1][0] == "!=":
+#     if run(p[1][1]) != run(p[1][2]):
+#         run(p[2])
