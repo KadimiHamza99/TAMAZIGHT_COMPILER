5f3e7911c639cc61037d219d19d077df29e5508b
KadimiHamza <hamza.kadimi@uit.ac.ma>
2022-01-08 22:16:36 +0100

    adding style to the ide

--- ide.py
+++ ide.py
@@ -3,16 +3,20 @@ from TamazightLexerParser import parser
 
 window = Tk()
 window.title('T++ IDE')
+window.geometry('900x850')
+window.config()
+
 
 def RunCode():
     print("Execution ...")
-    code = textEditor.get('1.0',END)
+    code = textEditor.get('1.0', END)
     parser.parse(code)
 
-textEditor = Text()
+
+textEditor = Text(window, highlightcolor = 'green', highlightbackground='green', fg='#c0c2e8', bg='#13153d',height=26, font=("verdana", 15))
 textEditor.pack()
 
-button = Button(window,text = 'ⵣⵣⴳⵔ - Run', command=RunCode)
-button.pack()
+button = Button(window, text='ⵣⵣⴳⵔ - Run', command=RunCode, bg='green')
+button.pack(pady=5)
 
 window.mainloop()
